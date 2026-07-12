fb88f47c3b5be3b1cf3734630745fe5f443759cb
jayavardhan-jaya <157962417+jayavardhan-jaya@users.noreply.github.com>
2026-07-12 10:27:41 +0530

    Update add_duplicates.py

--- etl/add_duplicates.py
+++ etl/add_duplicates.py
@@ -1,31 +1,26 @@
 import pandas as pd
 
-# -------------------------------
-# Read Original Dataset
-# -------------------------------
+
 df = pd.read_excel("data/raw/placement_data.xlsx")
 
 print("Original Rows :", len(df))
 
-# -------------------------------
-# Randomly Select 150 Existing Rows
-# -------------------------------
+
 duplicate_rows = df.sample(
     n=150,
     random_state=42
 )
 
-# -------------------------------
+
 # Append Duplicate Rows
-# -------------------------------
+
 df_with_duplicates = pd.concat(
     [df, duplicate_rows],
     ignore_index=True
 )
 
-# -------------------------------
 # Save Dataset
-# -------------------------------
+
 output_file = "data/raw/placement_data_duplicates.xlsx"
 
 df_with_duplicates.to_excel(
